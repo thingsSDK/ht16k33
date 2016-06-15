079b42ff69769e823a21f5c2e2745a907e198698
pedesen <pedesen@googlemail.com>
2016-06-15 23:08:23 +0200

    add support for 16x8 LED matrix

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /**
- * Constructor for an Adafruit 8 x 8 LED Matrix Backpack.
+ * Constructor for Adafruit LED Matrix Backpacks which can be used as a base for 8x8 and 16x8 matrices.
  * Provides drawing capabilities like the Adafruit Arduino Library API.
  *
  * @param options. An object literal with the keys of `scl` and `sda` for the clock and data I2C pins. The option `address`
  * is a hexadecimal address of the device. A 7-bit address between 0x70-0x77. The key `brightness` is a number 0 through 15.
  * @constructor
  */
-function Matrix8x8(options) {
+function Matrix(options) {
     if(typeof options.scl === "undefined" ||
         typeof options.sda === "undefined" ||
         typeof options.brightness === "undefined" ||
@@ -21,24 +21,24 @@ function Matrix8x8(options) {
     this.context = Graphics.createArrayBuffer(16,8,1);
 }
 
+
 /**
  * Set the brightnes of the LEDs. A number 0 through 15.
  * @param brightness
  */
-Matrix8x8.prototype.setBrightness = function(brightness) {
+Matrix.prototype.setBrightness = function(brightness) {
     // brightness 0-15
     I2C1.writeTo(this.address, 0xE0 | brightness);
 };
 
+
 /**
  * Renders buffer/graphics context to the display.
  */
-Matrix8x8.prototype.render = function() {
-    I2C1.writeTo(this.address, 0,
-        //Because of how the 8x8 Matrix is wired you need to rotate right by one bit
-        (new Uint8Array(this.context.buffer)).map(this.rotate)
-    );
+Matrix.prototype.render = function() {
+    I2C1.writeTo(this.address, 0, this.context.buffer);
 };
+
 
 /**
  * Rotates an 8-bit binary value right one bit.
@@ -48,7 +48,7 @@ Matrix8x8.prototype.render = function() {
  * @param value. 8-bit unsidgned integer (0-127).
  * @returns {uint8} rotated value
  */
-Matrix8x8.prototype.rotate = function(value) {
+Matrix.prototype.rotate = function(value) {
     //Shift everything right 1 bit
     //Then shift last bit over if switched on it'll switch on 2^7
     var rotated = (value >> 1) | (value << 7);
@@ -58,19 +58,81 @@ Matrix8x8.prototype.rotate = function(value) {
     return eightBitArray[0];
 };
 
+
 /**
  * Clears graphics buffer.
  */
-Matrix8x8.prototype.clear = function() {
+Matrix.prototype.clear = function() {
     this.context.clear();
 };
+
 
 /**
  * Same as `render()`.
  */
-Matrix8x8.prototype.writeDisplay = function(){
+Matrix.prototype.writeDisplay = function(){
     this.render();
 };
+
+
+/**
+ * Draws a pixel at the co-ordinates.
+ * @param x is the x co-ordinate. 0-7.
+ * @param y is the y co-ordinate. 0-7.
+ * @param state. 1 for on. 0 for off.
+ */
+Matrix.prototype.drawPixel = function (x, y, state) {
+    this.context.setPixel(x, y, state);
+};
+
+
+/**
+ * Draws a line from one set of co-ordinates to another
+ *
+ * @param x1 is the x value for the first set of co-ordinates
+ * @param y1 is the y value for the first set of co-ordinates
+ * @param x2 is the x value for the second set of co-ordinates
+ * @param y2 is the y value for the second set of co-ordinates
+ */
+Matrix.prototype.drawLine = function(x1, y1, x2, y2) {
+    this.context.drawLine(x1, y1, x2, y2);
+};
+
+
+/**
+ * Draws a rectangle at a set of co-ordinates of a given width and height
+ *
+ * @param x is the x value where you want to start
+ * @param y is the y value where you want to start
+ * @param width is the width of the rectangle
+ * @param height is the height of the rectangle
+ */
+Matrix.prototype.drawRect = function(x, y, width, height) {
+    this.context.drawRect(x, y,  x + width - 1, y + height - 1);
+};
+
+
+/**
+ * Draws a filled in rectangle at a set of co-ordinates of a given width and height
+ * @param x is the x value where you want to start
+ * @param y is the y value where you want to start
+ * @param width is the width of the rectangle
+ * @param height is the height of the rectangle
+ */
+Matrix.prototype.fillRect = function(x, y, width, height) {
+    this.context.fillRect(x, y, x + width - 1, y + height - 1);
+};
+
+
+/**
+ * Constructor for an Adafruit 8x8 LED Matrix Backpack.
+ */
+function Matrix8x8(options) {
+    Matrix.call(this, options);
+}
+
+Matrix8x8.prototype = Object.create(Matrix.prototype);
+Matrix8x8.prototype.constructor = Matrix;
 
 
 /**
@@ -86,49 +148,50 @@ Matrix8x8.prototype.drawBitmap = function(uint8array){
     this.context.buffer = new Uint8Array(array);
 };
 
-/**
- * Draws a pixel at the co-ordinates.
- * @param x is the x co-ordinate. 0-7.
- * @param y is the y co-ordinate. 0-7.
- * @param state. 1 for on. 0 for off.
- */
-Matrix8x8.prototype.drawPixel = function (x, y, state) {
-    this.context.setPixel(x, y, state);
-};
 
 /**
- * Draws a line from one set of co-ordinates to another
- *
- * @param x1 is the x value for the first set of co-ordinates
- * @param y1 is the y value for the first set of co-ordinates
- * @param x2 is the x value for the second set of co-ordinates
- * @param y2 is the y value for the second set of co-ordinates
+ * Renders buffer/graphics context to the display.
  */
-Matrix8x8.prototype.drawLine = function(x1, y1, x2, y2) {
-    this.context.drawLine(x1, y1, x2, y2);
-};
-
-/**
- * Draws a rectangle at a set of co-ordinates of a given width and height
- *
- * @param x is the x value where you want to start
- * @param y is the y value where you want to start
- * @param width is the width of the rectangle
- * @param height is the height of the rectangle
- */
-Matrix8x8.prototype.drawRect = function(x, y, width, height) {
-    this.context.drawRect(x, y,  x + width - 1, y + height - 1);
-};
-
-/**
- * Draws a filled in rectangle at a set of co-ordinates of a given width and height
- * @param x is the x value where you want to start
- * @param y is the y value where you want to start
- * @param width is the width of the rectangle
- * @param height is the height of the rectangle
- */
-Matrix8x8.prototype.fillRect = function(x, y, width, height) {
-    this.context.fillRect(x, y, x + width - 1, y + height - 1);   
+Matrix8x8.prototype.render = function() {
+    I2C1.writeTo(this.address, 0,
+       //Because of how the 8x8 Matrix is wired you need to rotate right by one bit
+       (new Uint8Array(this.context.buffer)).map(this.rotate)
+   );
 };
 
 module.exports.Matrix8x8 = Matrix8x8;
+
+
+/**
+ * Constructor for an Adafruit 16x8 LED Matrix Backpack.
+ */
+function Matrix16x8(options) {
+    Matrix.call(this, options);
+}
+
+Matrix16x8.prototype = Object.create(Matrix.prototype);
+Matrix16x8.prototype.constructor = Matrix;
+
+
+/**
+ * Writes an array of 16 8-bit values to the display graphics context
+ *
+ * The first two bytes of the array represent the first line and so on.
+ * This: drawBitmap([0b00000000, 0b1111111, 0b00001111, 0b00110011, ...])
+ * ends up like this in a 16x8 Matrix:
+ *
+ * 0000000011111111
+ * 0000111100110011
+ * ...
+ *
+ * @param uint8array
+ */
+Matrix16x8.prototype.drawBitmap = function(uint8array) {
+    var array = [];
+    uint8array.forEach(function(i){
+        array.push(i);
+    });
+    this.context.buffer = new Uint8Array(array);
+};
+
+module.exports.Matrix16x8 = Matrix16x8;
